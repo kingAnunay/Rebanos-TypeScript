5d517e0fd33fc4b29c0a41ac124550d979f741bf
kingAnunay <72414708+kingAnunay@users.noreply.github.com>
2020-11-17 21:21:54 +0530

    Changed something but forgot.But Important

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,22 +1,36 @@
 import React from "react";
-import "./Header.css";
 
 import { AppBar, Toolbar, IconButton, Typography} from "@material-ui/core";
 import Menu from "@material-ui/icons/Menu";
 
 import { Input , Avatar } from '@material-ui/core';
 
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderBottom: "2px Solid Black",
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    padding: '0 1px',
+  },
+});
 
 export default function Header(){
+  const classes=useStyles();
   return(
     <AppBar 
-      position="sticky" 
+      // position="sticky" 
+      className={classes.root}
       style={{backgroundColor:"Black"}}
+      
       >
       <Toolbar>
         <Typography
           style={{color:"Green",
-          paddingRight:"10px"
+          paddingRight:"5px",
+          fontSize:"20px",
           }}
         >
           Rebanos
@@ -25,13 +39,19 @@ export default function Header(){
             placeholder="Search"
             style={{color:"Black",
             backgroundColor:"White",
+            border:"1px solid grey",
             borderRadius:"5px",
-            padding:"3px 10px"}}
+            padding:"1px 10px",
+            width:"100%",
+          }}
           />
         <IconButton>
-          <Avatar/>
+          <Avatar
+            style={{border:"2px solid White"}}
+          />
           <Menu
-            style={{color:"White"}}/>
+            style={{color:"White",
+              paddingLeft:"10px"}}/>
         </IconButton>    
       </Toolbar>  
     </AppBar>
